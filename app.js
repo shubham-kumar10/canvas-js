4b6e5392955c47c1913e94ca09d164ee7e21bf33
shubham.snu@gmail.com <shubham.snu@gmail.com>
2020-12-20 16:28:17 +0530

    feature: added canvas

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 window.addEventListener("load", ()=>{
     const canvas= document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
-
+    console.log('Hello')
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    ctx.fillRect(50,50,200,200);
+    // ctx.strokeRect(50,50,200,200);
+
+    let painting = false;
+
+    function startPosition(){
+        painting = true;
+    }
+
+    function endPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(event){
+        if(!painting)
+            return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.lineTo(event.clientX, event.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(event.clientX,event.clientY);
+    }
+
+    canvas.addEventListener('mousedown',startPosition);
+    canvas.addEventListener('mouseup',endPosition);
+    canvas.addEventListener('mousemove',draw);
 })
